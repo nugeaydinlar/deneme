31335ce9d9500c1c40a92503d565230bdfdcbf32
nugeaydinlar1821012094 <nugeaydnlar@gmail.com>
2022-10-20 23:24:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/navbar/Header';
+import Slider from './components/slider/Slider';
+
+function App() {
+  return (
+    <div>
+  <Header />
+  <Slider />
+    </div>
+ 
+  );
+}
+
+export default App;
